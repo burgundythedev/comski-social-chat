378ac9ab0998daf7445ff8c5b1327c82810ceee7
burgundythedev <bourgogne.olivier@gmail.com>
2024-04-18 13:32:35 +0200

    :construction_worker: change url front end domain and set up for deploy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+require("dotenv").config();
+
 const userRoute = require("./routes/userRoute");
 const chatRoute = require("./routes/chatRoute");
 const userMessages = require("./routes/messageRoute");
 
 // Set up Express
 const app = express();
-app.use(express.json());
+
+// Security enhancements
+app.use(helmet());
+
+// CORS Configuration
 app.use(
   cors({
     origin: ["https://broski-social-chat.onrender.com"],
@@ -15,24 +25,27 @@ app.use(
   })
 );
 
-// Load environment variables
-require("dotenv").config();
+// Middleware for parsing JSON
+app.use(express.json());
 
-// Define routes
+// Serve static files - only if you're serving your front-end from the same server
+
+app.use(express.static("/home/olivierb/comski-social-chat/client/dist"));
+
+// API routes
 app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", userMessages);
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Welcome to Social Media App");
+// Make sure all non-API requests return the index.html file, enabling SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join("/home/olivierb/comski-social-chat/client/dist", "index.html")
+  );
 });
 
 // Set up HTTP server and Socket.IO
-const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-
-const onlineUsers = [];
 const io = new Server(server, {
   cors: {
     origin: ["https://broski-social-chat.onrender.com"],
@@ -41,40 +54,37 @@ const io = new Server(server, {
   },
 });
 
+const onlineUsers = [];
+
 io.on("connection", (socket) => {
-  console.log("new connection:", socket.id);
+  console.log("New connection:", socket.id);
 
   socket.on("addNewUser", (userID) => {
     const userExists = onlineUsers.some((user) => user.userID === userID);
-
     if (!userExists) {
       onlineUsers.push({ userID, socketID: socket.id });
     } else {
       const userIndex = onlineUsers.findIndex((user) => user.userID === userID);
       onlineUsers[userIndex].socketID = socket.id;
     }
-
-    console.log(onlineUsers);
     io.emit("getOnlineUsers", onlineUsers);
   });
+
   socket.on("sendMessage", ({ chatId, senderId, text, receiverId }) => {
     const receiver = onlineUsers.find((user) => user.userID === receiverId);
     if (receiver) {
-      const message = {
+      io.to(receiver.socketID).emit("receiveMessage", {
         chatId,
         senderId,
         text,
         unread: true,
-      };
-
-      io.to(receiver.socketID).emit("receiveMessage", message);
-
+      });
       io.to(receiver.socketID).emit("unreadMessage", { chatId, unread: true });
     }
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("User disconnected");
     const userIndex = onlineUsers.findIndex(
       (user) => user.socketID === socket.id
     );
@@ -86,11 +96,22 @@ io.on("connection", (socket) => {
 // MongoDB connection
 const uri = process.env.ATLAS_URI;
 mongoose
-  .connect(uri)
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     console.log("MongoDB database connection established successfully")
   )
-  .catch((error) => console.log("MongoDb connection error: ", error.message));
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+// Error handling for unmatched routes or methods
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // Start the server
 const PORT = process.env.PORT || 4000;
